6c1c74c790ec4ac7d9b8b62437fe4612722330e0
m3o-actions <>
2021-11-02 16:31:07 +0000

    Commit from m3o/m3o action

--- examples/event/README.md
+++ examples/event/README.md
@@ -51,3 +51,24 @@ async function subscribeToAtopic() {
 
 subscribeToAtopic()
 ```
+## Read
+
+Read stored events
+
+
+[https://m3o.com/event/api#Read](https://m3o.com/event/api#Read)
+
+```js
+const { EventService } = require('m3o/event');
+
+// Read stored events
+async function readEventsOnAtopic() {
+	let eventService = new EventService(process.env.M3O_API_TOKEN)
+	let rsp = await eventService.read({
+  "topic": "user"
+})
+	console.log(rsp)
+}
+
+readEventsOnAtopic()
+```
--- src/event/index.ts
+++ src/event/index.ts
@@ -14,6 +14,10 @@ export class EventService {
       request
     ) as Promise<PublishResponse>;
   }
+  // Read stored events
+  read(request: ReadRequest): Promise<ReadResponse> {
+    return this.client.call("event", "Read", request) as Promise<ReadResponse>;
+  }
   // Subscribe to messages for a given topic.
   subscribe(request: SubscribeRequest): Promise<SubscribeResponse> {
     return this.client.call(
@@ -22,6 +26,15 @@ export class EventService {
       request
     ) as Promise<SubscribeResponse>;
   }
+}
+
+export interface Ev {
+  // event id
+  id?: string;
+  // event message
+  message?: { [key: string]: any };
+  // event timestamp
+  timestamp?: string;
 }
 
 export interface PublishRequest {
@@ -33,16 +46,36 @@ export interface PublishRequest {
 
 export interface PublishResponse {}
 
+export interface ReadRequest {
+  // number of events to read; default 25
+  limit?: number;
+  // offset for the events; default 0
+  offset?: number;
+  // topic to read from
+  topic?: string;
+}
+
+export interface ReadResponse {
+  // the events
+  events?: Ev[];
+}
+
 export interface SubscribeRequest {
   // Optional group for the subscription
   group?: string;
+  // Optional offset to read from e.g "2006-01-02T15:04:05.999Z07:00"
+  offset?: string;
   // The topic to subscribe to
   topic?: string;
 }
 
 export interface SubscribeResponse {
+  // Unique message id
+  id?: string;
   // The next json message on the topic
   message?: { [key: string]: any };
+  // Timestamp of publishing
+  timestamp?: string;
   // The topic subscribed to
   topic?: string;
 }
